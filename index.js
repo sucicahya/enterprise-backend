bfa4ea3e579a177a410357ea43ae5a219033cd7c
sucicahya <100661444+sucicahya@users.noreply.github.com>
2024-08-11 15:16:05 +0700

    second

--- index.js
+++ index.js
@@ -13,62 +13,26 @@
 //     console.log('Server-running on port 3000');
 // });
 
-
-const express = require('express');
 const sql = require('msnodesqlv8');
-const cors = require('cors');
-const app = express();
-const port = 5000;
 
 const connectionString = 'Driver={ODBC Driver 17 for SQL Server};Server=40005-MP1PWS22;Database=enterprise;Trusted_Connection=yes;';
 
-// sql.open(connectionString, (err, conn) => {
-//   if (err) {
-//     console.error('Error occurred:', err);
-//     return;
-//   }
 
-//   const query = 'SELECT * FROM produk'; // Replace with your table name
+sql.open(connectionString, (err, conn) => {
+  if (err) {
+    console.error('Error occurred:', err);
+    return;
+  }
 
-//   conn.query(query, (err, results) => {
-//     if (err) {
-//       console.error('Error executing query:', err);
-//     } else {
-//       console.log(results);
-//     }
+  const query = 'SELECT * FROM produk'; // Replace with your table name
 
-//     conn.close();
-//   });
-// });
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// API Endpoint
-app.get('/api/produk', (req, res) => {
-  sql.open(connectionString, (err, conn) => {
+  conn.query(query, (err, results) => {
     if (err) {
-      console.error('Error occurred:', err);
-      res.status(500).send('Database connection error');
-      return;
+      console.error('Error executing query:', err);
+    } else {
+      console.log(results);
     }
 
-    const query = 'SELECT * FROM produk';
-    conn.query(query, (err, results) => {
-      if (err) {
-        console.error('Error executing query:', err);
-        res.status(500).send('Query execution error');
-      } else {
-        res.json(results);
-      }
-
-      conn.close();
-    });
+    conn.close();
   });
-});
-
-// Start Server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
 });
