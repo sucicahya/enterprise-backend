057ab515a6a0d2fb06961f3d061949a3028e0463
sucicahya <100661444+sucicahya@users.noreply.github.com>
2024-08-12 16:11:13 +0700

    kurang post

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(cors());
 app.use(express.json());
 
 // API Endpoint
-app.get('/api/produk', (req, res) => {
+app.get('/main-table', (req, res) => {
   sql.open(connectionString, (err, conn) => {
     if (err) {
       console.error('Error occurred:', err);
@@ -54,7 +54,162 @@ app.get('/api/produk', (req, res) => {
       return;
     }
 
-    const query = 'SELECT * FROM produk';
+    const query = 'SELECT produk.ID_PRODUK, produk.NAMA_PRODUK, produk_detail.URL, spec_server.IP_SERVER, karyawan.NAMA, status.NAMA_STATUS FROM produk INNER JOIN produk_detail ON produk.ID_PRODUK = produk_detail.PRODUK_ID INNER JOIN spec_server ON produk_detail.SERVER = spec_server.ID_SPEC_SERVER INNER JOIN karyawan ON produk_detail.PIC_NIPPOS = karyawan.NIPPOS INNER JOIN status ON produk.FLAG_STATUS = status.ID_STATUS';
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Query execution error');
+      } else {
+        res.json(results);
+      }
+
+      conn.close();
+    });
+  });
+});
+
+app.get('/penempatan-cloud', (req, res) => {
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error('Error occurred:', err);
+      res.status(500).send('Database connection error');
+      return;
+    }
+
+    const query = "SELECT penempatan.NAMA_PENEMPATAN AS penempatan, COUNT(produk.NAMA_PRODUK) AS total FROM penempatan LEFT JOIN produk_detail ON penempatan.ID_PENEMPATAN = produk_detail.PENEMPATAN LEFT JOIN produk ON produk.ID_PRODUK = produk_detail.PRODUK_ID WHERE penempatan.NAMA_PENEMPATAN = 'Cloud' GROUP BY penempatan.NAMA_PENEMPATAN;";
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Query execution error');
+      } else {
+        res.json(results);
+      }
+
+      conn.close();
+    });
+  });
+});
+
+app.get('/penempatan-onprem', (req, res) => {
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error('Error occurred:', err);
+      res.status(500).send('Database connection error');
+      return;
+    }
+
+    const query = "SELECT penempatan.NAMA_PENEMPATAN AS penempatan, COUNT(produk.NAMA_PRODUK) AS total FROM penempatan LEFT JOIN produk_detail ON penempatan.ID_PENEMPATAN = produk_detail.PENEMPATAN LEFT JOIN produk ON produk.ID_PRODUK = produk_detail.PRODUK_ID WHERE penempatan.NAMA_PENEMPATAN = 'On-Premise' GROUP BY penempatan.NAMA_PENEMPATAN;";
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Query execution error');
+      } else {
+        res.json(results);
+      }
+
+      conn.close();
+    });
+  });
+});
+
+app.get('/status-aktif', (req, res) => {
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error('Error occurred:', err);
+      res.status(500).send('Database connection error');
+      return;
+    }
+
+    const query = "SELECT status.NAMA_STATUS AS status, COUNT(produk.NAMA_PRODUK) AS total FROM status LEFT JOIN produk ON produk.FLAG_STATUS = status.ID_STATUS WHERE status.NAMA_STATUS = 'Aktif' GROUP BY status.NAMA_STATUS;";
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Query execution error');
+      } else {
+        res.json(results);
+      }
+
+      conn.close();
+    });
+  });
+});
+
+app.get('/status-nonaktif', (req, res) => {
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error('Error occurred:', err);
+      res.status(500).send('Database connection error');
+      return;
+    }
+
+    const query = "SELECT status.NAMA_STATUS AS status, COUNT(produk.NAMA_PRODUK) AS total FROM status LEFT JOIN produk ON produk.FLAG_STATUS = status.ID_STATUS WHERE status.NAMA_STATUS = 'Non-Aktif' GROUP BY status.NAMA_STATUS;";
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Query execution error');
+      } else {
+        res.json(results);
+      }
+
+      conn.close();
+    });
+  });
+});
+
+app.get('/total', (req, res) => {
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error('Error occurred:', err);
+      res.status(500).send('Database connection error');
+      return;
+    }
+
+    const query = "SELECT COUNT(NAMA_PRODUK) AS total FROM produk;";
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Query execution error');
+      } else {
+        res.json(results);
+      }
+
+      conn.close();
+    });
+  });
+});
+
+app.get('/produk-masuk', (req, res) => {
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error('Error occurred:', err);
+      res.status(500).send('Database connection error');
+      return;
+    }
+
+    const query = "SELECT COUNT(produk.NAMA_PRODUK) AS total FROM produk LEFT JOIN produk_detail ON produk.ID_PRODUK = produk_detail.PRODUK_ID WHERE DATEPART(MONTH, produk_detail.TANGGAL_DEPLOY) = DATEPART(MONTH, GETDATE()) AND DATEPART(YEAR, produk_detail.TANGGAL_DEPLOY) = DATEPART(YEAR, GETDATE());";
+    conn.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Query execution error');
+      } else {
+        res.json(results);
+      }
+
+      conn.close();
+    });
+  });
+});
+
+app.post('/full-detail', (req, res) => {
+  const { id } = req.body;
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error('Error occurred:', err);
+      res.status(500).send('Database connection error');
+      return;
+    }
+
+    const query = "SELECT produk.NAMA_PRODUK, produk.DESKRIPSI_PRODUK, account.USERNAME, account.PASS, account.TANGGAL_CREATE, account.TANGGAL_UPDATE, account.EXP_DATE_PASSWORD, account.JENIS_AKUN, akses.NAMA_AKSES, developer.NAMA_DEVELOPER, karyawan.NAMA, karyawan.TELEPON, penempatan.NAMA_PENEMPATAN, status.NAMA_STATUS, produk_detail.PIC_NIPPOS, produk_detail.BUSINESS_OWNER, produk_detail.WAKTU_OPERASIONAL, produk_detail.URL, produk_detail.PORT, produk_detail.FRAMEWORK, produk_detail.VER_FRAMEWORK, produk_detail.JENIS_DATABASE, produk_detail.TANGGAL_LIVE, produk_detail.TANGGAL_AKHIR_UPDATE, produk_detail.TANGGAL_TUTUP, produk_detail.TANGGAL_DEPLOY, produk_detail.BA_DEPLOY, produk_detail.REQ_DEPLOY, spec_server.IP_SERVER, spec_server.CPU, spec_server.RAM, spec_server.STORAGE, web_server.NAMA_WEB_SERVER FROM produk_detail INNER JOIN produk ON produk.ID_PRODUK = produk_detail.PRODUK_ID INNER JOIN status ON produk.FLAG_STATUS = status.ID_STATUS INNER JOIN account ON produk.ID_PRODUK = account.PRODUK_ID INNER JOIN akses ON produk_detail.AKSES = akses.ID_AKSES INNER JOIN developer ON produk_detail.DEVELOPER = developer.ID_DEVELOPER INNER JOIN karyawan ON karyawan.NIPPOS = produk_detail.PIC_NIPPOS INNER JOIN penempatan ON produk_detail.PENEMPATAN = penempatan.ID_PENEMPATAN INNER JOIN spec_server ON produk_detail.SERVER = spec_server.ID_SPEC_SERVER INNER JOIN web_server ON spec_server.WEB_SERVER_ID = web_server.ID_WEB_SERVER WHERE PRODUK_DETAIL.ID_PRODUK_DETAIL = ${ id };";
     conn.query(query, (err, results) => {
       if (err) {
         console.error('Error executing query:', err);
